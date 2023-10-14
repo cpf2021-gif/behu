b3f39cc228fc19b1d382ca860363b39923e9c77c
cpf2021-gif <2992247892@qq.com>
2023-10-14 17:00:09 +0800

    feat(like): Implement thumbup service with kafka

---
+++ application/like/mq/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	"behu/application/like/mq/internal/config"
+	"behu/application/like/mq/internal/logic"
+	"behu/application/like/mq/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+)
+
+var configFile = flag.String("f", "etc/like.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+
+	svcCtx := svc.NewServiceContext(c)
+	ctx := context.Background()
+	serviceGroup := service.NewServiceGroup()
+	defer serviceGroup.Stop()
+
+	for _, mq := range logic.Consumers(ctx, svcCtx) {
+		serviceGroup.Add(mq)
+	}
+
+	serviceGroup.Start()
+}
